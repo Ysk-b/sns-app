14efaf847c32c93d71a09355478dd460e12b705c
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 01:06:43 +0900

    Amend API for tweeting

--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
-const cors = require("cors");
+const postsRoute = require("./routers/posts");
 
 require("dotenv").config();
+const cors = require("cors");
 
 const PORT = 8000;
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
