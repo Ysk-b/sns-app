8fb22db32e4ec6c05010fd8ef836017ee72f8ee4
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-11 15:53:58 +0900

    Timelineコンポーネントの実装

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Timeline from './components/Molecules/Timeline'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Timeline />
+    </div>
   )
 }
 
