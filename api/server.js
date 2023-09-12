464ef2dc5ab734bab4d790ed96f07086a7cfe568
kawabata.blam <y.kawabata@blam.co.jp>
2023-09-12 19:41:04 +0900

    modify path to routers/post

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
-const postsRoute = require("./routers/posts");
+const postsRoute = require("./routers/post");
+// const usersRoute = require("./routers/user")
+const cors = require("cors");
 
 require("dotenv").config();
-const cors = require("cors");
 
 const PORT = 8000;
 
@@ -12,5 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
+// app.use("/api/users", usersRoute);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- client/src/app/components/Molecules/NavBar.tsx
+++ client/src/app/components/Molecules/NavBar.tsx
@@ -7,31 +7,31 @@ interface Props {
 
 const NavBar = () => {
   return (
-<header className="bg-gray-700 p-4 text-white">
-  <div className="container mx-auto flex justify-between items-center">
-    <h1 className="font-semibold text-xl">
-      <Link href="/">SNS Clone</Link>
-    </h1>
-    <nav>
-      <ul className="flex space-x-4">
-        <>
-          <Link
-            href="/login"
-            className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
-          >
-            ログイン
-          </Link>
-          <Link
-            href="/signup"
-            className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
-          >
-            サインアップ
-          </Link>
-        </>
-      </ul>
-    </nav>
-  </div>
-</header>
+    <header className='bg-gray-700 p-4 text-white'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <h1 className='font-semibold text-xl'>
+          <Link href='/'>SNS Clone</Link>
+        </h1>
+        <nav>
+          <ul className='flex space-x-4'>
+            <>
+              <Link
+                href='/pages/login'
+                className='bg-white text-gray-900 py-2 px-3 rounded-lg font-medium'
+              >
+                ログイン
+              </Link>
+              <Link
+                href='/pages//signup'
+                className='bg-white text-gray-900 py-2 px-3 rounded-lg font-medium'
+              >
+                サインアップ
+              </Link>
+            </>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
